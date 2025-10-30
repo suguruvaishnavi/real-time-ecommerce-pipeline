181f294f0045a6654b33630a7f1eecda6b35ed51
Vaishnavi Suguru <vsuguru@unomaha.edu>
2025-10-30 15:38:58 -0400

    Add changes to poll_sales.py file

--- scripts/poll_sales.py
+++ scripts/poll_sales.py
@@ -1,29 +1,20 @@
-# Step 1: Import helpers
-import requests  # This helps Python talk to websites
-import time      # This helps Python wait or pause
 
-# Step 2: Remember the API website
-API_URL = "https://dummyjson.com/products"  # This is where we get fake product data
+import requests, time
 
-# Step 3: Make a helper machine (function) to get data
-def get_sales_data():
+API_URL = "https://dummyjson.com/products"
 
-    # Ask the website for data
+def get_products_data():
     response = requests.get(API_URL)
-
-    # If something goes wrong, stop the program and tell us
     response.raise_for_status()
-
-    # Convert the website data into Python language (list/dictionary)
     data = response.json()
-
-    # Return the data so the rest of the program can use it
     return data
 
-# Step 4: Keep asking for data forever
 while True:
-    # Call our helper machine to get fresh sales data
-    sales_data = get_sales_data()
+    products_data = get_products_data()
+    print("🛒 New Products Data Arrived!")
 
-    # Print a fun message to know new data arrived
-    print("🛒 New Sales Data Arrived!")
+    for item in products_data['products']:
+        print(f"Product: {item['title']}, Price: ${item['price']}")
+        print("Waiting 5 seconds before polling again...\n")
+        time.sleep(5)
+
